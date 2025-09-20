03c1d0b1e2dbe1e33d550a2a89954366a51adf95
Lucas <lucas.silveriosousa@gmail.com>
2025-09-20 14:40:27 -0300

    Companies CRUD

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function index()
+    {
+        $companies = Company::all();
+
+        return view('companies.index', compact('companies'));
+    }
+
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    public function store(StoreCompanyRequest $request)
+    {
+        Company::create($request->validated());
+
+        return to_route('companies.index');
+    }
+
+    public function edit(Company $company)
+    {
+        return view('companies.edit', compact('company'));
+    }
+
+    public function update(UpdateCompanyRequest $request, Company $company)
+    {
+        $company->update($request->validated());
+
+        return to_route('companies.index');
+    }
+
+    public function destroy(Company $company)
+    {
+        $company->delete();
+
+        return to_route('companies.index');
+    }
+
+}
